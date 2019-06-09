384f0a7f956cf448665f65932b11e95e4f68ff50
isc-wre <rene.winkler@isc-ejpd.admin.ch>
2019-06-09 10:48:45 +0200

    burger finished

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,10 +4,14 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey}/>)
-        });
+        }).reduce((arr, el) => arr.concat(el), []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = "Please start adding ingredients";
+    }
 
     return (
         <div className={classes.Burger}>
